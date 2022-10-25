4600a495463ace9136dc880f987776fc23e90553
4elovel <marcynakvova007@gmail.com>
2022-10-25 17:58:55 +0300

    Добавьте файлы проекта.

---
+++ Stack_2/Stack_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stack>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list <int> ls{ 1,2,3,4,5,6,7,8,9,10 };
+
+    stack <int, list<int>> st(ls);
+
+	while (st.top()!=7)
+	{
+		cout << "Number " << st.top() << " is on top now\n";
+		st.pop();
+	}
+}
